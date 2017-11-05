3a5211941f6d214d4e35c0fe5f712fe887452670
cherry <991939332@qq.com>
2017-11-05 17:54:57 +0800

    update

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,28 +1,31 @@
-require('./check-versions')()
+// 检查 Nodejs 和 npm 版本
+require('./check-versions')();
 
-var config = require('../config')
+// 获取基本配置
+var config = require('../config');
+// 如果 Node 的环境变量中没有设置当前的环境（NODE_ENV），则使用config中的dev环境配置作为当前的环境
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
 
-var opn = require('opn')
-var path = require('path')
-var express = require('express')
-var webpack = require('webpack')
-var proxyMiddleware = require('http-proxy-middleware')
-var webpackConfig = require('./webpack.dev.conf')
+var opn = require('opn');            // opn 是一个可以调用默认软件打开网址、图片、文件等内容的插件，使用 opn 自动打开 dev-server 坚挺的端口
+var path = require('path');          // nodejs 的一个 api，提供了一些文件路径的操作方法
+var express = require('express');    // 是 nodejs 的一个框架，我们使用 express  启动一个 web server
+var webpack = require('webpack');    // 引入 webpack
+var proxyMiddleware = require('http-proxy-middleware');      // http-proxy-middleware是一个express中间件，用于将http请求代理到其他服务器
+var webpackConfig = require('./webpack.dev.conf');           // 开发环境下的 webpack 配置
 
 // default port where dev server listens for incoming traffic
-var port = process.env.PORT || config.dev.port
+var port = process.env.PORT || config.dev.port;              // dev-server 监听的端口，如果没有在命令行传入端口号，则使用config.dev.port设置的端口，例如8080
 // automatically open browser, if not set will be false
-var autoOpenBrowser = !!config.dev.autoOpenBrowser
+var autoOpenBrowser = !!config.dev.autoOpenBrowser;          // 用于判断是否要自动打开浏览器的布尔变量，当配置文件中没有设置自动打开浏览器的时候其值为 false
 // Define HTTP proxies to your custom API backend
 // https://github.com/chimurai/http-proxy-middleware
-var proxyTable = config.dev.proxyTable
+var proxyTable = config.dev.proxyTable;                      // HTTP 代理表，指定规则，将某些API请求代理到相应的服务器
 
-var app = express();
+var app = express();                                         // 创建 express 服务器
 
-var appData = require('../data.json');
+var appData = require('../data.json');                       // 引入 mock 使用的数据
 var seller = appData.seller;
 var goods = appData.goods;
 var ratings = appData.ratings;
@@ -52,19 +55,20 @@ apiRoutes.get('/goods', function (req, res) {
 
 app.use('/api', apiRoutes);
 
-var compiler = webpack(webpackConfig);
+var compiler = webpack(webpackConfig);                                  // webpack 根据配置开始编译打包源码并返回 compiler 对象
 
-var devMiddleware = require('webpack-dev-middleware')(compiler, {
-  publicPath: webpackConfig.output.publicPath,
-  quiet: true
+var devMiddleware = require('webpack-dev-middleware')(compiler, {       // webpack-dev-middleware将webpack编译打包后得到的产品文件存放在内存中而没有写进磁盘
+  publicPath: webpackConfig.output.publicPath,                          // 设置访问路径为 webpack 配置中的 output 里面所对应的路径
+  quiet: true                                                           // 设置为true，使其不要在控制台输出日志
+});
+
+var hotMiddleware = require('webpack-hot-middleware')(compiler, {       // webpack-hot-middleware，用于实现热重载功能的中间件
+  log: false,                                                           // 关闭控制台的日志输出
+  heartbeat: 2000                                                       // 发送心跳包的频率
 })
 
-var hotMiddleware = require('webpack-hot-middleware')(compiler, {
-  log: false,
-  heartbeat: 2000
-})
 // force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
+compiler.plugin('compilation', function (compilation) {                 // webpack(重新)编译打包完成后并将js、css等文件inject到html文件之后，通过热重载中间件强制页面刷新
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
     hotMiddleware.publish({action: 'reload'})
     cb()
@@ -72,50 +76,50 @@ compiler.plugin('compilation', function (compilation) {
 })
 
 // proxy api requests
-Object.keys(proxyTable).forEach(function (context) {
-  var options = proxyTable[context]
+Object.keys(proxyTable).forEach(function (context) {                    // 根据 proxyTable 中的代理请求配置来设置express服务器的http代理规则
+  var options = proxyTable[context];
   if (typeof options === 'string') {
-    options = {target: options}
+    options = {target: options};                                        // 格式化 options，例如将'www.example.com'变成{ target: 'www.example.com' }
   }
   app.use(proxyMiddleware(options.filter || context, options))
-})
+});
 
 // handle fallback for HTML5 history API
-app.use(require('connect-history-api-fallback')())
+app.use(require('connect-history-api-fallback')());                     // 重定向不存在的 URL，用于支持 SPA（单页应用）
 
 // serve webpack bundle output
-app.use(devMiddleware)
+app.use(devMiddleware);                                                 // 挂载 webpack-dev-middleware 中间件，提供 webpack 编译打包后的产品文件服务
 
 // enable hot-reload and state-preserving
 // compilation error display
-app.use(hotMiddleware)
+app.use(hotMiddleware);                                                 // 挂载热重载中间件
 
 // serve pure static assets
-var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
-app.use(staticPath, express.static('./static'))
+var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory);      // 提供 static 文件夹上的静态文件服务
+app.use(staticPath, express.static('./static'));
 
-var uri = 'http://localhost:' + port
+var uri = 'http://localhost:' + port;              // 访问链接
 
-var _resolve
+var _resolve;                                      // 创建promise，在应用服务启动之后resolve,便于外部文件require了这个dev-server之后的代码编写
 var readyPromise = new Promise(resolve => {
   _resolve = resolve
 })
 
-console.log('> Starting dev server...')
-devMiddleware.waitUntilValid(() => {
-  console.log('> Listening at ' + uri + '\n')
+console.log('> Starting dev server...');            // webpack-dev-middleware 等待 webpack 完成所有编译打包之后输出提示语到控制台，表明服务正式启动
+devMiddleware.waitUntilValid(() => {                // 服务正式启动才自动打开浏览器进入页面
+  console.log('> Listening at ' + uri + '\n');
   // when env is testing, don't need open it
   if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
     opn(uri)
   }
   _resolve()
-})
+});
 
-var server = app.listen(port)
+var server = app.listen(port);                       // 启动express服务器并监听相应的端口
 
-module.exports = {
+module.exports = {                                   // 暴露本模块的功能给外部使用
   ready: readyPromise,
   close: () => {
     server.close()
   }
-}
+};
--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,15 +3,16 @@ var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 
+// 返回绝对路径
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
 module.exports = {
-  entry: {
+  entry: {                                                     // webpack 文件入口
     app: './src/main.js'
   },
-  output: {
+  output: {                                                    // webpack 输出路径
     path: config.build.assetsRoot,
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
@@ -19,51 +20,54 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
+    // 自动补全文件路径
     extensions: ['.js', '.vue', '.json'],
+    // 提供别名，缩短路径长度
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
+      'vue$': 'vue/dist/vue.esm.js',                           // import Vue from 'vue/dist/vue.common.js'可以写成 import Vue from 'vue'
       '@': resolve('src'),
     }
   },
+  // 不同类型模块的处理规则
   module: {
     rules: [
       {
         test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        enforce: 'pre',
-        include: [resolve('src'), resolve('test')],
+        loader: 'eslint-loader',                               // 对src和test文件夹下的 .js 和 .vue 文件使用 eslint-loader 进行代码规范检查
+        enforce: 'pre',                                        // 在 loder 之前进行处理
+        include: [resolve('src'), resolve('test')],            // 只对此目录下的文件经行处理
         options: {
           formatter: require('eslint-friendly-formatter')
         }
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: 'vue-loader',                                  // 对所有.vue文件使用vue-loader进行编译
         options: vueLoaderConfig
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: 'babel-loader',                                // 对 src 和 test 文件夹下的 .js 文件使用 babel-loader 将 es6+ 的代码转成 es5
         include: [resolve('src'), resolve('test')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: 'url-loader',                                  // 对图片资源文件使用url-loader
         options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+          limit: 10000,                                        // 图片小于 10kb 的情况转化为 base64
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')  // 其他的图片转移到静态资源文件夹
         }
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: 'url-loader',                                  // 对多媒体资源文件使用url-loader
         options: {
           limit: 10000,
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
       {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,               // 对字体资源文件使用url-loader
         loader: 'url-loader',
         options: {
           limit: 10000,
